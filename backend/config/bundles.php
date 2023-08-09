32f08fd01476fd8737284ed0471b73b072e4b687
Patrick Heller <patrick.heller@digistore24.team>
2023-08-09 12:37:37 +0200

    uses roadrunner for application serving

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Baldinof\RoadRunnerBundle\BaldinofRoadRunnerBundle::class => ['all' => true],
 ];
