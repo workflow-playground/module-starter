e5412c14a722a961dbec315652f1143d26d35a6a
Patrick Heller <patrick.heller@digistore24.team>
2023-08-09 15:12:28 +0200

    waits for db to be ready

---
+++ wait-until.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# See https://github.com/nickjj/wait-until
+
+command="${1}"
+timeout="${2:-30}"
+
+echo "Waiting for \"${command}\" ..."
+
+i=1
+until eval "${command}"
+do
+    ((i++))
+
+    if [ "${i}" -gt "${timeout}" ]; then
+        echo "command was never successful, aborting due to ${timeout}s timeout!"
+        exit 1
+    fi
+
+    sleep 1
+done
