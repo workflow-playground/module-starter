d396077ba882cd078ea50efacf5a145bddcc29e5
Alexander Heimbuch <alexander.heimbuch@digistore24.team>
2023-08-14 15:47:23 +0200

    feat: add e2e

---
+++ e2e/cypress/selectors/login.ts
@@ -0,0 +1,5 @@
+import { select } from './helper';
+
+export const usernameInput = select('login-username');
+export const passwordInput = select('login-password');
+export const formSubmit = select('login-submit');
